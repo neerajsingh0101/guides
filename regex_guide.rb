8e058c03a828b3f3087ef79d49860195fac3b573
Neeraj Singh <neerajdotname@gmail.com>
2009-12-28 02:02:33 -0500

    fixing typos

--- regex_guide.rb
+++ regex_guide.rb
@@ -23,9 +23,9 @@ puts m.to_a
 #President
 
 
-
 # dynamic find_by methods in ActiveRecord
 if method_sym.to_s =~ /^find_by_(.*)$/ 
+
 
 # extract phone numbers from given string
 s = %Q{  (123) 555-1234. }
@@ -33,37 +33,19 @@ r = /\((\d{3})\)\s+(\d{3})-(\d{4})/
 m = r.match(s)
 puts m.to_a
 
+
 # edge case
 s = %Q{abc}
 r = /((.)(.)(.))/
 m = r.match(s)
-puts m.to_a
+puts m.to_a.inspect #=> ["abc", "abc", "a", "b", "c"]
+puts '***'
 puts m[1] #=> abc
 # question is why value of m[1] is abc and not a. well the regular expression engine see a bracket and continues
 # to be in that bracket until that bracket ends. In this case the outer most bracket spans the whole string. So
 # the first value is assigned to the full string. Then the engine moves on to the inner brackets and accordingly
 # m[2] m[3] and m[4] are assigned.
 
-
-
-# begin and end method could be used to debug regex when user finds that group capture is not yielding right
-# result
-#
-# MatchData.begin 
-# Returns the offset of the start of the nth element of the match array in the string
-s = %Q{THX1138.}
-r = /(.)(.)(\d+)(\d)/
-m = r.match(s)
-puts m[0] #=> HX1138
-
-puts m[1] #=> H
-puts  m.begin(1)   #=> 1
-
-puts m[2] #=> X
-puts  m.begin(2)   #=> 2
-
-puts m[3] #=> 113
-puts  m.begin(3) #=> 3
 
 
 # greedy case
@@ -89,7 +71,7 @@ puts /\d+?/.match('there is a digit 3456 here') #=> 3
 m =  /(\d{3})-(\d{3})-(\d{4})/.match('123-456-7890')
 puts m.to_a.inspect #=> ["123-456-7890", "123", "456", "7890"]
 #you can also specify
-#\d{1,10} between a and 10 digits
+#\d{1,10} between 1 and 10 digits
 #\d{7,} 7 ore more digits
 
 
@@ -97,9 +79,10 @@ puts m.to_a.inspect #=> ["123-456-7890", "123", "456", "7890"]
 m = /([A-Z]){5}/.match('David BLACK')
 puts m.to_a.inspect #=> ["BLACK", "K"]
 
+
 m = /([A-Z]{5})/.match('David BLACK')
 puts m.to_a.inspect #=> ["BLACK", "BLACK"]
-# In the first case the captured value is K. In the second case captured value is BLACK. So pay close
+# In the first case the captured value is K. In the second case captured value is BLACK. Pay close
 # attention if braces are inside the capture or outside the capture.
 
 
@@ -210,7 +193,7 @@ puts s.sub(/(\w+):(\w+)/,'\2, \1') #=> Smith, Mike
 <div >Hello World</div>
 
 
-# turning a greedy match into non greedy match
+# turning a greedy match into non greedy match with ?
 s = ', 1 , 2 , 3 , 4'
 puts s[/,(.*),/,1] #=> 1 , 2, 3
 puts s[/,(.*?),/,1] #=> 1
@@ -223,3 +206,25 @@ puts s[/,(.*?),/,1] #=> 1
 text = "\nhello \n\n world \n "
 puts text.gsub(/([^\n]\n)(?=[^\n])/, '\1<br />')
 text.gsub!(/\n\n+/, "</p>\n\n#{start_tag}")
+
+# begin and end method could be used to debug regex when user finds that group capture is not yielding right
+# result
+#
+# MatchData.begin 
+# Returns the offset of the start of the nth element of the match array in the string
+s = %Q{THX1138.}
+r = /(.)(.)(\d+)(\d)/
+m = r.match(s)
+puts m.to_a.inspect #=> ["HX1138", "H", "X", "113", "8"]
+puts m[0] #=> HX1138
+
+puts m[1] #=> H
+puts  m.begin(1)   #=> 1
+
+puts m[2] #=> X
+puts  m.begin(2)   #=> 2
+
+puts m[3] #=> 113
+puts  m.begin(3) #=> 3
+
+
