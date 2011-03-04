d00204c848460ac594f3edd03d5378ef1e7f1c99
Neeraj Singh <neeraj@railsdog.com>
2011-03-04 16:58:27 -0500

    done

--- ruby_regex.rb
+++ ruby_regex.rb
@@ -4,7 +4,7 @@ else
   # Match attempt failed
 end
 
-#This code does exactly the same thing:
+#This code does exactly the same thing as above
 if /regex pattern/ =~ subject
   # Successful match
 else
@@ -13,10 +13,12 @@ end
 
 
 
-# two simple ways to handle string and regex
+# two simple ways to get captured value
 s = 'total: 3'
+puts s.match(/(total): (\d+)/)[0] #=> total: 3
+puts s.match(/(total): (\d+)/)[1] #=> total
 puts s.match(/(total): (\d+)/)[2] #=> 3
-puts s[/(total): (\d+)/,2] #=> 3
+puts s[/(total): (\d+)/, 2] #=> 3
 
 
 # find everything within double quotes
@@ -28,7 +30,7 @@ puts /"(.*)"/.match(s)[1] # will give you the string without the double quotes
 # extract last_name,first_name,country, title from a string
 s = %Q{Clinton,Bill,USA,President}
 m = /([A-Za-z]+),([A-Za-z]+),([A-Za-z]+),([A-Za-z]+)/.match(s)
-puts m.to_a
+puts m
 #Clinton,Bill,USA,President
 #Clinton
 #Bill
@@ -45,14 +47,18 @@ s = %Q{  (123) 555-1234. }
 r = /\((\d{3})\)\s+(\d{3})-(\d{4})/
 m = s.match(r)
 puts m.to_a
+#(123) 555-1234
+#123
+#555
+#1234
+
 
 
 # edge case
-s = %Q{abc}
+s = "abc"
 r = /((.)(.)(.))/
 m = r.match(s)
 puts m.to_a.inspect #=> ["abc", "abc", "a", "b", "c"]
-puts '***'
 puts m[1] #=> abc
 # question is why value of m[1] is abc and not a. well the regular expression engine see a bracket and continues
 # to be in that bracket until that bracket ends. In this case the outer most bracket spans the whole string. So
@@ -60,10 +66,9 @@ puts m[1] #=> abc
 # m[2] m[3] and m[4] are assigned.
 
 
-
 # greedy case
-puts /\d+/.match('there is a digit 3 here') #=> 3
-puts /\d+/.match('there is a digit 3456 here') #=> 3456
+puts /\d+/.match('there is a digit 3 here')[0] #=> 3
+puts /\d+/.match('there is a digit 3456 here')[0] #=> 3456
 # + means one ore more matches. In the first line single digit 3 was matched. In the second case 3456 was
 # matched. In the second line when regex engine encountered 3 then the condition that there should be one
 # or more digit is satisfied was met. Regex engine could have stopped here. But following two regex operators
@@ -74,7 +79,7 @@ puts /\d+/.match('there is a digit 3456 here') #=> 3456
 # Because they are greedy, it has consequences.
 puts /.+!/.match('abc!def!ghi!') #=> abc!def!ghi!
 # In the above case I wanted to extract just abc but because + is greedy it gobbled up whole string.
-# ? is away to tell engine to not to be greedy. ? works at a lot of places including
+# ? is a way to tell engine to not to be greedy. ? works at a lot of places including
 # ‹*?›, ‹+?›, ‹??›, and ‹{7,42}?
 puts /.+?!/.match('abc!def!ghi!') #=> abc!
 puts /\d+?/.match('there is a digit 3456 here') #=> 3
@@ -83,10 +88,11 @@ puts /\d+?/.match('there is a digit 3456 here') #=> 3
 # another example of greedy case made not greedy by using ?
 s = '<p>hello</p><p>world</p>'
 regex = /<p>(.*)<\/p>/
-# above regex will capture
-# hello</p><p>world
-# to fix it make it non greedy
+puts regex.match(s).to_a #=> ["<p>hello</p><p>world</p>", "hello</p><p>world"]
+
+# In the above case if you want to pick just hello then make the regex non-greedy.
 regex = /<p>(.*?)<\/p>/
+puts regex.match(s).to_a #=> ["<p>hello</p>", "hello"]
 
 
 
